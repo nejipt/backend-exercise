3d4cb0c257d8e5cf3cd0324783d6fb001a209ec7
nejipt <aj.20.cunha@gmail.com>
2021-07-02 15:09:51 +0100

    initial commit

---
+++ test/dummy.test.js
@@ -0,0 +1,7 @@
+describe('someTest', () => {
+  test('dummy - test', () => {
+    expect(true).toStrictEqual(true)
+    expect('some string').toStrictEqual('some string')
+    expect(0).toStrictEqual(0)
+  })
+})
